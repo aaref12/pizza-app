6328d6c8d7f4626fad51d59d0b06499d31fb0d6c
shaikh aaref hamza <skaarefz@gmail.com>
2025-07-04 14:54:52 +0530

    some changes

--- clint-pizza/src/app/(home)/page.tsx
+++ clint-pizza/src/app/(home)/page.tsx
@@ -3,11 +3,11 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main>
+    <>
    <h1>welcome to pizza shope</h1>
    <Button >button</Button>
    
 
-    </main>
+    </>
   );
 }
---
+++ clint-pizza/src/components/custom/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Header = () => {
+    return<header className="bg-white">
+        <nav className="container py-5 items-center">  
+         navbar
+        </nav>
+    </header>
+
+}
+export default Header;
